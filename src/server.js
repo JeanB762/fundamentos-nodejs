5a54264ba6661a8180f092e18e7f2eb797f1ee01
Jean Borges <jeanborges946@gmail.com>
2023-01-10 12:07:19 -0300

    feature: Criação da verificação dos métodos HTTP e configuração dos Headers da resposta

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import http from "node:http";
+
+const users = [];
+
+const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users")
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@mail.example.com",
+    });
+
+    return response.end("criação de usuários");
+  }
+
+  return response.end("Hello World!");
+});
+
+server.listen(3333);
